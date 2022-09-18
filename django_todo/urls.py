11da001ee94f3a4782c4f05e9b8987979bf1da98
Matt <mjd014@live.co.uk>
2022-09-18 14:26:24 +0000

    added code comments explaining what is happening

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# Importing our say_hello function from views.py
 from todo.views import say_hello
 
 urlpatterns = [
