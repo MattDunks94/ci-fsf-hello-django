8399fc62e3abbd2bf86336df1e2bc2b8c7b356ea
Matt <mjd014@live.co.uk>
2022-09-18 16:48:58 +0000

    created our own 'model' Item, with name and done fields, added code comments explaining the process

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    # CharField(), imported from models, text only method.
+    # max_length set to 50 characters.
+    # null and blank makes sure the name field is not empty.
+    name = models.CharField(max_length=50, null=False, blank=False)
+    # BooleanField(), imported from models, is either TRUE or FALSE.
+    # default sets the item to not done, TRUE meaning it's done.
+    done = models.BooleanField(null=False, blank=False, default=False)
