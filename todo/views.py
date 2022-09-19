7cb99aaeef881f96c2ca1d50795d5316bd03a5d9
Matt <mjd014@live.co.uk>
2022-09-19 14:16:51 +0000

    added items variable to our function, imported our Item model from models.py

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from .models import Item
 # Create your views here.
 
 
 def get_todo_list(request):
-    # This will be displayed on the page.
-    return render(request, 'todo/todo_list.html')
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    # This will be displayed on the browser.
+    return render(request, 'todo/todo_list.html', context)
