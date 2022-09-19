fe745db4dbf17e2afe06b1265b812a856efa27e1
Matt <mjd014@live.co.uk>
2022-09-19 15:00:52 +0000

    add if statement to our add_item function, adds user data to Item class.

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
 # Create your views here.
 
@@ -13,5 +13,12 @@ def get_todo_list(request):
 
 
 def add_item(request):
+    # Receives user data, new item, and adds it to Item class
+    if request.method == "POST":
+        name = request.POST.get("item_name")
+        done = "done" in request.POST
+        Item.objects.create(name=name, done=done)
+
+        return redirect('get_todo_list')
     # This will be displayed on the browser.
     return render(request, 'todo/add_item.html')
