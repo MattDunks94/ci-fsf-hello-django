92731f9c2f601b9efa1f8180c44052def02a10f9
Matt <mjd014@live.co.uk>
2022-09-19 17:22:04 +0000

    added code comments explaining the form process.

--- todo/models.py
+++ todo/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 
+# 'name' and 'done' are our input fields for our form.
 class Item(models.Model):
     # CharField(), imported from models, text only method.
     # max_length set to 50 characters.
@@ -10,7 +11,8 @@ class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     # BooleanField(), imported from models, is either TRUE or FALSE.
     # default sets the item to not done, TRUE meaning it's done.
+    # Displays checkbox.
     done = models.BooleanField(null=False, blank=False, default=False)
-    # Overiding django string method, displaying own item name
+    # Overiding django string method, displaying own item name.
     def __str__(self):
         return self.name
