342dfdf346d4f2bec7a254f5ef7b529752219a15
Matt <mjd014@live.co.uk>
2022-09-20 18:45:30 +0000

    created new function, edit_item, allows user to edit current instances from the class Item.

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Item
 from .forms import ItemForm
 # Create your views here.
@@ -30,3 +30,25 @@ def add_item(request):
     }
     # This will be displayed on the browser.
     return render(request, 'todo/add_item.html', context)
+
+
+def edit_item(request, item_id):
+    # The get_object_or_404 method returns a class if it exists.
+    # In this case, collecting our Item class, as first parameter.
+    # Second parameter is our item_id from todo_list.html, button.
+    item = get_object_or_404(Item, id=item_id)
+    if request.method == "POST":
+        # Instance accesses the objects within a class (Item)
+        # This pre-populates the form with the current item details
+        form = ItemForm(request.POST, instance=item)
+        # is_valid() method runs validation, returns a boolean
+        # If it returns TRUE it saves user data
+        if form.is_valid():
+            # save() saves the form data into Item, the model, forms.py
+            form.save()
+            return redirect('get_todo_list')
+    form = ItemForm(instance=item)
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/edit_item.html', context)
