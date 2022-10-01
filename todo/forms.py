68b0d55c7c9527e4c63561e5368521939bf13c98
Matt <mjd014@live.co.uk>
2022-10-01 20:16:30 +0000

    added code comments.

--- todo/forms.py
+++ todo/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Item
 
-
+# Creates form for adding item with 2 fields: name, done.
 class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
