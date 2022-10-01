3d0f80d68fe8d63c0f9a0ace6935c5e35c489919
Matt <mjd014@live.co.uk>
2022-10-01 19:27:22 +0000

    created new function, delete_item, removes chosen item from list.

--- todo/views.py
+++ todo/views.py
@@ -60,3 +60,10 @@ def toggle_item(request, item_id):
     item.done = not item.done
     item.save()
     return redirect('get_todo_list')
+
+
+# Deletes chosen item.
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.delete()
+    return redirect('get_todo_list')
