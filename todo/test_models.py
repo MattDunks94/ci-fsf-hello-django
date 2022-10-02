233bc5d8e87f4b13e2a583683cbc2bc5ed17a1e2
Matt <mjd014@live.co.uk>
2022-10-02 14:25:48 +0000

    added new test, testing the done status of our item is false as default.

--- todo/test_models.py
+++ todo/test_models.py
@@ -1,3 +1,14 @@
 # TestCase is an extension of the Python standard library module.
 from django.test import TestCase
+from .models import Item
+
+
+class TestModels(TestCase):
+
+    # Testing the done status is False as default.
+    def test_done_defaults_to_false(self):
+        # Creating new 'dummy' item with name.
+        item = Item.objects.create(name='Test Todo Item')
+        # Checking whether the done status is False.
+        self.assertFalse(item.done)
 
