4199658e9f965d65bc8e5720c324f90fc63f1692
Matt <mjd014@live.co.uk>
2022-10-02 11:43:19 +0000

    created new test, testing the edit path view.

--- todo/test_views.py
+++ todo/test_views.py
@@ -1,5 +1,6 @@
 # TestCase is an extension of the Python standard library module.
 from django.test import TestCase
+from .models import Item
 
 
 class TestViews(TestCase):
@@ -10,12 +11,18 @@ class TestViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'todo/todo_list.html')
 
+    # Testing the '/add' view is equal to status code 200, successful reponse.
     def test_get_add_item_page(self):
         response = self.client.get('/add')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'todo/add_item.html')
 
-    # def test_get_edit_item_page(self):
+    # Testing the '/edit' view is equal to status code 200, successful reponse.
+    def test_get_edit_item_page(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.get(f'/edit/{item.id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/edit_item.html')
 
     # def test_can_add_item(self):
 
