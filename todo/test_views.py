48e170b97a7d58e60b5132b3519c271b071907b5
Matt <mjd014@live.co.uk>
2022-10-02 11:35:09 +0000

    created two new tests, testing our views.

--- todo/test_views.py
+++ todo/test_views.py
@@ -1,2 +1,26 @@
 # TestCase is an extension of the Python standard library module.
 from django.test import TestCase
+
+
+class TestViews(TestCase):
+
+    # Testing the HTTP responses.
+    def test_get_todo_list(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/todo_list.html')
+
+    def test_get_add_item_page(self):
+        response = self.client.get('/add')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/add_item.html')
+
+    # def test_get_edit_item_page(self):
+
+    # def test_can_add_item(self):
+
+    # def test_can_delete_item(self):
+
+    # def test_can_toggle_item(self):
+    
+    
